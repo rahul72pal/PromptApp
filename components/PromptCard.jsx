bae70795a72f42386c5336ee38c8cc418353dda7
Rahul <rahulgwl72@gmail.com>
2024-03-02 15:17:23 +0530

    update promptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -66,7 +66,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 onClick={() => handleTagClick && handleTagClick(post.tag)}
             >#{post?.tag}</p>
 
-            {session?.user.id === post.creator._id && pathName === '/profile' && (
+            {session?.user.id === post?.creator?._id && pathName === '/profile' && (
                 <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
                     <p className='font-inter text-sm green_gradient cursor-pointer'
                         onClick={handleEdit}
